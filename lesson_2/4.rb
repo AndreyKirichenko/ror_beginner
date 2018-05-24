6e81d45670e3c513fc038dc9ea65308c97542635
Andrey Kirichenko <andrey.kirichenko999@gmail.com>
2018-05-24 18:22:08 +0300

    lesson 2

---
+++ lesson_2/4.rb
@@ -0,0 +1,14 @@
+# Заполнить хеш гласными буквами, где значением будет являтся порядковый номер буквы в алфавите (a - 1).
+
+vowels = 'aeiouy'
+letters = ("a".."z").to_a
+
+vowels_with_order = {}
+
+letters.each_with_index do |letter, index|
+  if vowels.include? letter
+    vowels_with_order[letter.to_sym] = index + 1
+  end
+end
+
+puts vowels_with_order
