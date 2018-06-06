7eec497977664e8a9e461d923277d1789fe2080c
Andrey Kirichenko <andrey.kirichenko999@gmail.com>
2018-06-06 23:12:40 +0300

    lesson 4

--- lesson_4/train.rb
+++ lesson_4/train.rb
@@ -1,19 +1,3 @@
-# Класс Train (Поезд):
-# Имеет номер (произвольная строка) и тип (грузовой, пассажирский) и количество вагонов,
-# эти данные указываются при создании экземпляра класса
-# Может набирать скорость
-# Может возвращать текущую скорость
-# Может тормозить (сбрасывать скорость до нуля)
-# Может возвращать количество вагонов
-# Может прицеплять/отцеплять вагоны (по одному вагону за операцию,
-# метод просто увеличивает или уменьшает количество вагонов).
-# Прицепка/отцепка вагонов может осуществляться только если поезд не движется.
-# Может принимать маршрут следования (объект класса Route).
-# При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте.
-# Может перемещаться между станциями, указанными в маршруте. Перемещение возможно вперед и назад,
-# но только на 1 станцию за раз.
-# Возвращать предыдущую станцию, текущую, следующую, на основе маршрута
-
 class Train
   attr_reader :number, :route, :speed, :station, :type, :wagons
 
@@ -59,21 +43,6 @@ class Train
     move_to_station route.stations[0]
   end
 
-  def move_to_station(station)
-    if @station
-      @station.departure self
-    end
-
-    return puts "Маршрут не задан. Движение не возможно" unless @route
-    if @station == station
-      puts "поезд уже находится на станции #{station.name}"
-      return false
-    end
-    @station = station
-    @station.arrive self
-    true
-  end
-
   def go_next
     current_index = route.stations.index @station
     next_index = current_index + 1
@@ -98,5 +67,22 @@ class Train
       move_to_station route.stations[next_index]
       true
     end
+  end
+
+  private
+
+  def move_to_station(station)
+    if @station
+      @station.departure self
+    end
+
+    return puts "Маршрут не задан. Движение не возможно" unless @route
+    if @station == station
+      puts "поезд уже находится на станции #{station.name}"
+      return false
+    end
+    @station = station
+    @station.arrive self
+    true
   end
 end
